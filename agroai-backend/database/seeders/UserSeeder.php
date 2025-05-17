0e679731692a2fb19dee63a2f70f5b628d9de508
Luka Janicijevic <janicijevicluka88@gmail.com>
2025-05-17 11:01:06 +0200

    Dodata administratorska rola

--- agroai-backend/database/seeders/UserSeeder.php
+++ agroai-backend/database/seeders/UserSeeder.php
@@ -4,11 +4,19 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
     public function run(): void
     {
+        User::create([
+            'name'       => 'Admin User',
+            'email'      => 'admin@agroai.com',
+            'password'   => Hash::make('adminagroai'),
+            'image_url'  => 'https://media.istockphoto.com/id/1265176370/photo/portrait-of-a-confident-young-businessman.jpg?s=612x612&w=0&k=20&c=Hr5Rn3WlBied-o4Qu2LiRc6wP_eHI8UMG9rl1v-_a9s=',
+            'role'       => 'administrator',
+        ]);
         // create 5 users
         User::factory()->count(5)->create();
     }
